d75748601e0854b43ce6b4aded1931772d58b44b
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-03-24 09:12:49 -0300

    feat: Actualización de la interfaz de usuario: Agregado de barra lateral y eliminación del componente HelloWorld

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
-import { inject, onMounted, ref } from 'vue';
-import { WindowFrame } from '@vasakgroup/vue-libvasak';
-import HelloWorld from './components/HelloWorld.vue';
+import { inject, onMounted, ref, computed } from 'vue';
+import { WindowFrame, SideBar, SideButton } from '@vasakgroup/vue-libvasak';
+import routers from '@/routers/index';
 
 const $vsk: any = inject('vsk');
 const icon = ref('');
+const section = ref('WELCOME');
+const routersData = ref(routers);
 
-const setIcon = async() => {
+const setIcon = async () => {
   icon.value = await $vsk.getIcon('calamares');
 };
+
+const changeSection = (newSection: string) => {
+  console.log('changeSection', newSection);
+  section.value = newSection;
+};
+
+const routerComponent = computed(() => {
+  return routers.find((router: any) => router.tag === section.value)?.component;
+});
+
+const getImage = (image: string): Promise<string> => $vsk.getIcon(image);
+
+routersData.value.forEach((router: any) => {
+  getImage(router.image).then((img: string) => {
+    router.image = img;
+  });
+});
 
 onMounted(() => {
   $vsk.loadUIConfig();
@@ -19,7 +38,21 @@ onMounted(() => {
 <template>
   <WindowFrame title="componere" v-bind:image="icon">
     <div class="flex">
-      <HelloWorld />
+      <SideBar>
+        <SideButton
+          v-for="router in routersData"
+          :key="router.tag"
+          :title="router.title"
+          :image="router.image"
+          @click="changeSection(router.tag)"
+        />
+      </SideBar>
+
+      <div class="p-3 col side-content-area">
+        <transition name="slide-fade" mode="out-in" appear>
+          <component :is="routerComponent"></component>
+        </transition>
+      </div>
     </div>
   </WindowFrame>
 </template>
