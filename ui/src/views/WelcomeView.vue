306776c153652de4659437767960cf981dac431c
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-03-24 10:31:49 -0300

    feat: Agrega verificación de conexión a internet en la vista de bienvenida

--- ui/src/views/WelcomeView.vue
+++ ui/src/views/WelcomeView.vue
@@ -1,8 +1,35 @@
 <script lang="ts" setup>
 import SlidesComponet from '@/components/SlidesComponet.vue';
+import { ref, onMounted, h } from 'vue';
+
+const timezone = ref('');
+const loading = ref(true);
+const haveInternet = ref(false);
+
+const checkInternet = async () => {
+  timezone.value = await fetch('http://ip-api.com/json/')
+    .then((response) => response.json())
+    .then((data) => {
+      haveInternet.value = true;
+      return data.timezone;
+    })
+    .catch(() => 'No internet');
+  loading.value = false;
+};
+
+onMounted(() => {
+  checkInternet();
+});
 </script>
 <template>
   <div>
     <SlidesComponet />
   </div>
-</template> 
+  <div v-if="loading">
+    <p>Loading...</p>
+  </div>
+  <div v-else>
+    <template v-if="haveInternet"> Tienes internet </template>
+    <template v-else> No tienes internet </template>
+  </div>
+</template>
