e19602b27405b609789cdcacb76bac294f4ca5b6
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-03-24 09:13:06 -0300

    feat: Agrega enrutador para la vista de bienvenida

---
+++ ui/src/routers/index.ts
@@ -0,0 +1,12 @@
+import WelcomeView from "../views/WelcomeView.vue";
+
+const routers: Array<any> = [
+  {
+    title: 'Welcome',
+    image: 'help-info',
+    tag: 'WELCOME',
+    component: WelcomeView
+  }
+];
+
+export default routers;
