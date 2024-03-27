c580b5a5d006e802b86c5ff4a82f671f4a4e720f
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-03-27 18:12:12 -0300

    feat: Añadir skel de creacion de usuario en UserView.vue

--- ui/src/views/UserView.vue
+++ ui/src/views/UserView.vue
@@ -1,7 +1,71 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { inject, ref, onMounted } from 'vue';
+import { installationConfigStore } from '@/stores/installationConfig';
+import type { VSK } from '@/types/VSK';
+
+const $vsk = inject('vsk') as VSK;
+const config = installationConfigStore();
+const image = ref('');
+const username = ref('');
+const password = ref('');
+
+onMounted(() => {
+  $vsk.getIcon('user_auth').then((img: string) => {
+    image.value = img;
+  });
+});
+</script>
 <template>
-  <div>
-    <h1>Users</h1>
-    <p>Users view</p>
+  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img class="mx-auto h-10 w-auto" :src="image" alt="Add user" />
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="#" method="POST">
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
+            >Usuario</label
+          >
+          <div class="mt-2">
+            <input
+              id="username"
+              name="username"
+              type="text"
+              required
+              v-model="username"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900"
+              >Password</label
+            >
+          </div>
+          <div class="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              required
+              v-model="password"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            ->
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
