c8ef334c8a11d65948c88ee3d9da1f8217bdf9de
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-03-27 16:53:13 -0300

    feat: Añadir vista de usuario

--- ui/src/routers/index.ts
+++ ui/src/routers/index.ts
@@ -1,12 +1,19 @@
-import WelcomeView from "../views/WelcomeView.vue";
-import type { VSKRoute } from "@/types/VSKRoute";
+import UserView from '@/views/UserView.vue';
+import WelcomeView from '../views/WelcomeView.vue';
+import type { VSKRoute } from '@/types/VSKRoute';
 
 const routers: Array<VSKRoute> = [
   {
     title: 'Welcome',
-    image: 'help-info',
+    image: 'start-here-archlinux',
     tag: 'WELCOME',
     component: WelcomeView
+  },
+  {
+    title: 'User',
+    image: 'user_icon',
+    tag: 'USER',
+    component: UserView
   }
 ];
 
---
+++ ui/src/views/UserView.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+<template>
+  <div>
+    <h1>Users</h1>
+    <p>Users view</p>
+  </div>
+</template>
