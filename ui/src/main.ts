23d78a35f32a967229c5770a403968ab104173bb
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-04-30 10:38:20 -0300

    Añadir iconos de FontAwesome en main.ts

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,9 +1,20 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+  faAngleLeft,
+  faAngleRight,
+  faUserPlus,
+  faDownload
+} from '@fortawesome/free-solid-svg-icons';
 
 const startApp = (channel: any) => {
   const app = createApp(App);
+  library.add(faAngleLeft, faAngleRight, faUserPlus, faDownload);
+
+  app.component('font-awesome-icon', FontAwesomeIcon);
   app.provide('vsk', channel.objects.vsk);
   app.use(createPinia());
 
