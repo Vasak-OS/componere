51a6bb9e350bc2e5f9dc12ea00e383793acf6ab3
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-05-01 09:44:32 -0300

    feat: Actualizar vista ConfirmView.vue para agregar funcionalidad de instalación y visualización de información de disco

--- ui/src/views/ConfirmView.vue
+++ ui/src/views/ConfirmView.vue
@@ -1,26 +1,62 @@
 <script lang="ts" setup>
-import { inject } from 'vue';
+import { inject, computed, ref, type Ref, onMounted } from 'vue';
+import CardComponent from '@/components/card/CardComponent.vue';
+import DiskSpaceComponent from '@/components/disk/DiskSpaceComponent.vue';
 import { installationConfigStore } from '@/stores/installationConfig';
+import { vskDiskPartitionDTO } from '@/utils/diskUtils';
 import type { VSK } from '@/types/VSK';
+import type { VSKDiskPartition, VSKDisk } from '@/types/VSKDisk';
+import { getDisks } from '@/utils/vasakFunctions';
 
 const config = installationConfigStore();
-const $vsk = inject('vsk') as VSK;
+const $vsk: VSK = inject('vsk') as VSK;
+const disks: Ref<Array<VSKDisk>> = ref([]);
 
-const install = async () => {
+const install = async (): Promise<void> => {
   const installData = JSON.stringify(config.config);
   const userData = JSON.stringify(config.userConfig);
   await $vsk.save(installData, 'config.json');
-  await $vsk.save(userData, 'user.json')
+  await $vsk.save(userData, 'user.json');
   await $vsk.install();
 };
+
+const setDisks = async (): Promise<void> => {
+  disks.value = await getDisks($vsk);
+  console.log(selectedDisk.value)
+};
+
+const emulatedFinalStatusPartitions = computed((): VSKDiskPartition[] | undefined => {
+  return config.config.disk_config.device_modifications[0].partitions.map((partition) => {
+    return vskDiskPartitionDTO(partition);
+  });
+});
+
+const selectedDisk = computed((): VSKDisk => {
+  disks
+  return (
+    disks.value.find(
+      (disk) => disk.deviceId === config.config.disk_config.device_modifications[0].device
+    ) || ({} as VSKDisk)
+  );
+});
+
+onMounted(() => {
+  setDisks();
+});
 </script>
 
 <template>
   <h1>Confirm</h1>
 
-  <pre><code>{{ config.config }}</code></pre>
+  <CardComponent>
+    <h2 class="card-title">Disk</h2>
+    <DiskSpaceComponent
+      :partitions="emulatedFinalStatusPartitions"
+      :diskSpace="selectedDisk?.size"
+    />
+  </CardComponent>
   <div class="componere-cta-section">
-    <button @click="$emit('prevSection')"><font-awesome-icon icon="fa-angle-left"/></button>
-    <button @click="install"><font-awesome-icon icon="fa-download"/></button>
+    <button @click="$emit('prevSection')"><font-awesome-icon icon="fa-angle-left" /></button>
+    <button @click="install"><font-awesome-icon icon="fa-download" /></button>
   </div>
 </template>
