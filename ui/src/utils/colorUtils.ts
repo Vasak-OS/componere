20a232df8e29dc7dc31e208c73ce211b85d74634
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-04-30 22:45:04 -0300

    Actualizar función randomColor para admitir diferentes formatos de sistema de archivos

--- ui/src/utils/colorUtils.ts
+++ ui/src/utils/colorUtils.ts
@@ -1,6 +1,12 @@
-export const randomColor = () => {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  return `rgb(${r}, ${g}, ${b})`;
+const swapColor = ['rgb(199,232,177)', 'rgb(255,155,155)'];
+const ext4Color = ['rgb(167,236,240)', 'rgb(226,172,249)', 'rgb(255,219,169)', 'rgb(142,195,177)'];
+const fatColor = ['rgb(198,137,64)', 'rgb(255,217,101)', 'rgb(59,152,185)'];
+
+export const randomColor = (format: string): string => {
+  console.log(format);
+  if (format === 'ext4' || format === 'Root')
+    return ext4Color[Math.floor(Math.random() * ext4Color.length)];
+  if (format === 'swap' || format === 'Swap')
+    return swapColor[Math.floor(Math.random() * swapColor.length)];
+  return fatColor[Math.floor(Math.random() * fatColor.length)];
 };
