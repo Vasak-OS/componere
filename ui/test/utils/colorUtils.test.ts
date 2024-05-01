88433dc784ad8a7daae6d89cd2ab0046e84523c1
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-04-30 22:45:26 -0300

    Actualizar pruebas de colorUtils para admitir diferentes formatos de sistema de archivos

--- ui/test/utils/colorUtils.test.ts
+++ ui/test/utils/colorUtils.test.ts
@@ -1,7 +1,27 @@
 import { randomColor } from '../../src/utils/colorUtils';
 import { expect, test } from '@jest/globals';
 
-test('Test Random Color', () => {
-  const color = randomColor();
-  expect(color).toMatch(/rgb\(\d{1,3}, \d{1,3}, \d{1,3}\)/);
+test('Test Random Color swap', () => {
+  const color = randomColor('swap');
+  expect(color).toMatch(/rgb\(\d{1,3},\d{1,3},\d{1,3}\)/);
+});
+
+test('Test Random Color ext4', () => {
+  const color = randomColor('ext4');
+  expect(color).toMatch(/rgb\(\d{1,3},\d{1,3},\d{1,3}\)/);
+});
+
+test('Test Random Color fat32', () => {
+  const color = randomColor('fat32');
+  expect(color).toMatch(/rgb\(\d{1,3},\d{1,3},\d{1,3}\)/);
+});
+
+test('Test Random Color Root', () => {
+  const color = randomColor('Root');
+  expect(color).toMatch(/rgb\(\d{1,3},\d{1,3},\d{1,3}\)/);
+});
+
+test('Test Random Color Swap', () => {
+  const color = randomColor('Swap');
+  expect(color).toMatch(/rgb\(\d{1,3},\d{1,3},\d{1,3}\)/);
 });
