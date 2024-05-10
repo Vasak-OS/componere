9d0242d2188276a6d68272004eda4b7b3c02aa9d
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-05-09 21:21:44 -0300

    feat: Agregar componente UserPictureComponent.vue para mostrar la imagen de usuario y permitir eliminar usuarios

---
+++ ui/src/components/user/UserPictureComponent.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+import { installationConfigStore } from '@/stores/installationConfig';
+import type { User } from '@/types/InstallationConfig';
+
+const props = defineProps<{
+  user: User;
+}>();
+
+const config = installationConfigStore();
+
+const deleteUser = () => {
+  config.deleteUser(props.user.username);
+};
+</script>
+
+<template>
+  <div class="componere-user-profile-area">
+    <img
+      :src="`https://ui-avatars.com/api/?name=${user.username}&background=random&size=64&bold=true&rounded=true`"
+      :alt="user.username"
+    />
+    <button class="componere-user-profile-button" @click="deleteUser">
+      <font-awesome-icon icon="fa-close" />
+    </button>
+  </div>
+</template>
