2abeec12803028c94757db34033fe589dd130c85
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-05-23 11:39:14 -0300

    feat: Agregar archivos de localización para los idiomas inglés, español, italiano y portugués

--- ui/src/views/WelcomeView.vue
+++ ui/src/views/WelcomeView.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { i18n } from '@/plugins/i18n';
 import SlidesComponet from '@/components/SlidesComponet.vue';
 import slides from '@/data/slides';
 import { ref, onMounted } from 'vue';
@@ -6,6 +7,7 @@ import { ref, onMounted } from 'vue';
 const timezone = ref('');
 const loading = ref(true);
 const haveInternet = ref(false);
+const { $t, SUPPORT_LOCALES, setI18nLanguage, locale } = i18n();
 
 const checkInternet = async () => {
   timezone.value = await fetch('http://ip-api.com/json/')
@@ -33,8 +35,20 @@ onMounted(() => {
     <template v-if="haveInternet"> Tienes internet </template>
     <template v-else> No tienes internet </template>
   </div>
+  <div>
+    <label for="language">{{ $t('welcome.language') }}</label>
+    <select id="language" @change="setI18nLanguage(($event.target as HTMLTextAreaElement).value)">
+      <option
+        v-for="localeSupported in SUPPORT_LOCALES"
+        :value="localeSupported"
+        :selected="localeSupported === locale"
+      >
+        {{ localeSupported }}
+      </option>
+    </select>
+  </div>
   <p>{{ timezone }}</p>
   <div class="componere-cta-section">
-    <button @click="$emit('nextSection')"><font-awesome-icon icon="fa-angle-right"/></button>
+    <button @click="$emit('nextSection')"><font-awesome-icon icon="fa-angle-right" /></button>
   </div>
 </template>
