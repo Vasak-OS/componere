73d13475fb25490da0d5ac8c130213519277c358
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-05-23 15:29:02 -0300

    feat: Actualizar vista de bienvenida y agregar soporte multilingüe

--- ui/src/views/WelcomeView.vue
+++ ui/src/views/WelcomeView.vue
@@ -2,19 +2,24 @@
 import { i18n } from '@/plugins/i18n';
 import SlidesComponet from '@/components/SlidesComponet.vue';
 import slides from '@/data/slides';
+import { installationConfigStore } from '@/stores/installationConfig';
+import type { Country } from '@/types/Country';
+import { getCountryByCode } from '@/utils/countryUtils';
 import { ref, onMounted } from 'vue';
 
-const timezone = ref('');
+const config = installationConfigStore();
 const loading = ref(true);
 const haveInternet = ref(false);
 const { $t, SUPPORT_LOCALES, setI18nLanguage, locale } = i18n();
 
 const checkInternet = async () => {
-  timezone.value = await fetch('http://ip-api.com/json/')
+  fetch('http://ip-api.com/json/')
     .then((response) => response.json())
     .then((data) => {
       haveInternet.value = true;
-      return data.timezone;
+      const country: Country = getCountryByCode(data.countryCode) as Country;
+      setI18nLanguage(country.officialLanguageCode);
+      config.setTimeZone(data.timezone) ;
     })
     .catch(() => 'No internet');
   loading.value = false;
@@ -28,13 +33,6 @@ onMounted(() => {
   <div>
     <SlidesComponet :slides="slides" />
   </div>
-  <div v-if="loading">
-    <p>Loading...</p>
-  </div>
-  <div v-else>
-    <template v-if="haveInternet"> Tienes internet </template>
-    <template v-else> No tienes internet </template>
-  </div>
   <div>
     <label for="language">{{ $t('welcome.language') }}</label>
     <select id="language" @change="setI18nLanguage(($event.target as HTMLTextAreaElement).value)">
@@ -47,7 +45,6 @@ onMounted(() => {
       </option>
     </select>
   </div>
-  <p>{{ timezone }}</p>
   <div class="componere-cta-section">
     <button @click="$emit('nextSection')"><font-awesome-icon icon="fa-angle-right" /></button>
   </div>
