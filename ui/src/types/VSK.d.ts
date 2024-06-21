e4cb41004dda68274a90dd8c1e6ccd6ba4d102eb
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-06-21 19:11:37 -0300

    feat: Refactor VSK module to use generatePartitions method

--- ui/src/types/VSK.d.ts
+++ ui/src/types/VSK.d.ts
@@ -5,10 +5,5 @@ export type VSK = {
   install: () => Promise<void>;
   getDisks: () => Promise<string>;
   getHardInfo: () => Promise<string>;
-  setDisk: (disk: string) => Promise<void>;
-  createUEFIBootPartition: (start: number, end: number) => Promise<void>;
-  createLegacyBootPartition: (start: number, end: number) => Promise<void>;
-  createSwapPartition: (start: number, end: number) => Promise<void>;
-  createRootPartition: (start: number, end: number) => Promise<void>;
-  createHomePartition: (start: number, end: number) => Promise<void>;
+  generatePartitions: () => Promise<void>;
 };
