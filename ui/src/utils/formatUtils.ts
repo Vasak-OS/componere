6a6d54d1109dcec12a3d6617e90bb846f230ddb4
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-06-21 19:12:24 -0300

    feat: Refactor disk formatting logic and improve partition creation

--- ui/src/utils/formatUtils.ts
+++ ui/src/utils/formatUtils.ts
@@ -4,46 +4,12 @@ import { getDisks } from './vasakFunctions';
 import type { VSKDisk } from '@/types/VSKDisk';
 
 export const formatDisk = async (disk: DiskConfig, vsk: VSK): Promise<DiskConfig> => {
-  await vsk.setDisk(disk.device_modifications[0].device);
-  for (const device of disk.device_modifications[0].partitions) {
-    await createPartition(device, vsk);
-  }
+  await vsk.generatePartitions();
   const newDisk: VSKDisk = (await getDisks(vsk)).find(
     (d) => d.path === disk.device_modifications[0].device
   ) as VSKDisk;
 
   return updateConfig(disk, newDisk);
-};
-
-const createPartition = async (device: Partition, vsk: VSK): Promise<void> => {
-  if (device.status === 'create') {
-    switch (device.mountpoint) {
-      case '/':
-        await vsk.createRootPartition(
-          mbToSectors(device.start.value),
-          mbToSectors(device.size.value)
-        );
-        break;
-      case '/home':
-        await vsk.createHomePartition(
-          mbToSectors(device.start.value),
-          mbToSectors(device.size.value)
-        );
-        break;
-      case 'swap':
-        await vsk.createSwapPartition(
-          mbToSectors(device.start.value),
-          mbToSectors(device.size.value)
-        );
-        break;
-      case '/boot':
-        await vsk.createUEFIBootPartition(
-          mbToSectors(device.start.value),
-          mbToSectors(device.size.value)
-        );
-        break;
-    }
-  }
 };
 
 const updateConfig = (disk: DiskConfig, newDisk: VSKDisk): DiskConfig => {
@@ -57,8 +23,4 @@ const updateConfig = (disk: DiskConfig, newDisk: VSKDisk): DiskConfig => {
     updatedDisk.device_modifications[0].partitions[i].obj_id = newPartitionData?.deviceId as string;
   }
   return updatedDisk;
-};
-
-const mbToSectors = (mb: number): number => {
-  return mb * 2048;
 };
