2a1bd2dd8978f75d59d4701ee4bd1b98bad0ee76
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-06-21 15:23:57 -0300

    feat: Add ComponerePartitionManager class for managing disk partitions

--- src/componere_binding.py
+++ src/componere_binding.py
@@ -5,6 +5,7 @@ from Vasak.system.vsk_icon_manager import VSKIconManager
 from Vasak.hardware.vsk_disks import VSKDisks
 from Vasak.hardware.vsk_info_hard import VSKInfoHard
 from Vasak.application.vsk_shell_connector import VSKShellConnector
+from componere_partition_manager import ComponerePartitionManager
 
 class ComponereBinding(QObject):
   def __init__(self, window, app):
@@ -15,6 +16,7 @@ class ComponereBinding(QObject):
     self.disks = VSKDisks()
     self.infoHard = VSKInfoHard()
     self.shell = VSKShellConnector()
+    self.partitionManager = ComponerePartitionManager()
 
   @pyqtSlot(result=str)
   def getHome(self):
@@ -95,6 +97,33 @@ class ComponereBinding(QObject):
   def getDisks(self):
     return str(self.disks.getDisksList())
   
-  @pyqtSlot( result=str)
+  @pyqtSlot(result=str)
   def getHardInfo(self):
     return json.dumps(self.infoHard.getInfo())
+  
+  @pyqtSlot(str)
+  def setDisk(self, disk):
+    self.partitionManager.setDisk(disk)
+    self.partitionManager.createPartitionList()
+  
+  @pyqtSlot(int ,int)
+  def createUEFIBootPartition(self, start, end):
+    self.partitionManager.createUEFIBootPartition(start, end)
+  
+  @pyqtSlot(int, int)
+  def createLegacyBootPartition(self, start, end):
+    self.partitionManager.createLegacyBootPartition(start, end)
+  
+  @pyqtSlot(int, int)
+  def createSwapPartition(self, start, end):
+    self.partitionManager.createSwapPartition(start, end)
+
+  @pyqtSlot(int, int)
+  def createRootPartition(self, start, end):
+    self.partitionManager.createRootPartition(start, end)
+  
+  @pyqtSlot(int, int)
+  def createHomePartition(self, start, end):
+    self.partitionManager.createHomePartition(start, end)
+
+  
--- ui/src/types/VSK.d.ts
+++ ui/src/types/VSK.d.ts
@@ -5,4 +5,10 @@ export type VSK = {
   install: () => Promise<void>;
   getDisks: () => Promise<string>;
   getHardInfo: () => Promise<string>;
+  setDisk: (disk: string) => Promise<void>;
+  createUEFIBootPartition: (start: number, end: number) => Promise<void>;
+  createLegacyBootPartition: (start: number, end: number) => Promise<void>;
+  createSwapPartition: (start: number, end: number) => Promise<void>;
+  createRootPartition: (start: number, end: number) => Promise<void>;
+  createHomePartition: (start: number, end: number) => Promise<void>;
 };
